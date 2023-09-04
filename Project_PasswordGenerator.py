fd1a134d6e4d756f99c5d39e75fa1a526ee204a8
Kieren Williamson <132315951+KierenWilliamson@users.noreply.github.com>
2023-09-04 13:15:45 -0400

    Update Project_PasswordGenerator.py
    Converted the older procedural design to a object-oriented design

--- Project_PasswordGenerator.py
+++ Project_PasswordGenerator.py
@@ -1,25 +1,75 @@
 import random
 
 
-def scramble(plain_password):
-    password_list = random.sample(plain_password, len(plain_password))
-    print("".join(password_list))
-    return
+class Password:
+    # lists containing the ASCII num ranges for their character categories
+    _fullcharlist = [[97, 122], [65, 90], [35, 38], [48, 57]]
 
+    # In the constructor the number of characters of each type is prompted
+    def __init__(self, lowercase_letters=1, uppercase_letters=1, symbols=1, numbers=1):
+        self.lowercase_letters = lowercase_letters
+        self.uppercase_letters = uppercase_letters
+        self.symbols = symbols
+        self.numbers = numbers
+        self.plaintext = ""
+        self.charnums = [self.lowercase_letters, self.uppercase_letters, self.symbols, self.numbers]
 
-def character_selector(low_range, high_range, count=2):
-    characters = ""
-    temp_list = random.sample(range(low_range, high_range+1), k=count)
-    for num in temp_list:
-        characters += chr(num)
-    return characters
+    # getters/setters
+    def get_lowercase(self):
+        return self.lowercase_letters
+
+    def get_uppercase(self):
+        return self.uppercase_letters
+
+    def get_symbols(self):
+        return self.symbols
+
+    def get_numbers(self):
+        return self.numbers
+
+    def get_characternums(self):
+        return self.charnums
+
+    def get_plaintext(self):
+        return self.plaintext
+
+    def set_lowercase(self, num):
+        self.lowercase_letters = num
+
+    def set_uppercase(self, num):
+        self.uppercase_letters = num
+
+    def set_symbols(self, num):
+        self.symbols = num
+
+    def set_numbers(self, num):
+        self.numbers = num
+
+    # This function is meant to accept a list and update the charnums list
+    def set_characternums(self, lowercase_letters=1, uppercase_letters=1, symbols=1, numbers=1):
+        self.charnums = [lowercase_letters, uppercase_letters, symbols, numbers]
+
+    def set_plaintext(self, new_text):
+        self.plaintext = new_text
+
+    # methods for password class
+    def assemble(self):
+        count = 0
+        for i in range(len(self._fullcharlist)):
+            plaintext_tokens = random.sample(range(self._fullcharlist[i][0], self._fullcharlist[i][1]),
+                                             k=self.charnums[count])
+            count += 1
+            for num in plaintext_tokens:
+                self.plaintext += chr(num)
+        return
+
+    def scramble(self):
+        self.assemble()
+        password_tokens = random.sample(self.plaintext, len(self.plaintext))
+        scrambled_password = "".join(password_tokens)
+        return scrambled_password
 
 
 if __name__ == "__main__":
-    new_password = ""
-    new_password += character_selector(97, 122, 9)
-    new_password += character_selector(65, 90, 4)
-    new_password += character_selector(48, 57, 3)
-    new_password += character_selector(35, 38, 2)
-    new_password += character_selector(48, 57, 5)
-    scramble(new_password)
+    secure_password = Password(2, 3, 1, 6)
+    print(secure_password.scramble())
